bc354af6298311624b6b094b0c6fcc77b0e8fa23
alice-odamtten <sakibeaodamtten@gmail.com>
2023-06-04 13:18:01 +0000

    Sun, Jun 4, 2023, 1:18 AM -12:00

--- mybank.c
+++ mybank.c
@@ -2,15 +2,15 @@
 
 int main(void)
 {
-    char firstname[50], lastname[50], othername[100], buf[3];
+    char firstname[50], lastname[50], othername[100], buf[4];
 
     printf("\nHello, Welcome To Jireh Bank. Banking in your home\n");
 
     //create account - implement registration
-    printf("creat an acount\n y or n\n");
+    printf("creat an account\n y or n\n");
     fgets(buf, sizeof(buf), stdin);
 
-    if (buf == 'y')
+    if (buf[0] == 'y')
     {
         printf("Enter first name: \n");
         fgets(firstname, sizeof(firstname), stdin);
